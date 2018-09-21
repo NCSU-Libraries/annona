7b6660d72aabea11f0607b85bcd5467498fe2b1c
Niqui O'Neill <doneill>
2018-09-21 16:38:40 -0400

    repo

--- iiif-annotation/src/build_dist.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import vueCustomElement from 'vue-custom-element'
-Vue.use(vueCustomElement);
-
-import iiifAnnotation from './components/iiifannotation.vue';
-Vue.customElement('iiif-annotation', iiifAnnotation);
