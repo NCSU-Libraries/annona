47640a7bc614f04fe03a560bed87090ce283ef04
Niqui O'Neill <doneill@ncsu.edu>
2019-07-23 19:23:11 -0400

    bug fix

--- src/components/storyboard.vue
+++ src/components/storyboard.vue
@@ -207,7 +207,7 @@ export default {
       //sets html dropdown to selected
       if(this.annotations.filter(element => element.languages).length > 0){
         var all_langs = this.annotations.map(element => element.textual_body.map(els => els.language)).flat();
-        var lang = all_langs.filter(element => navigator.language.indexOf(element.toLowerCase()) > -1)
+        var lang = all_langs.filter(element => element != undefined && navigator.language.indexOf(element.toLowerCase()) > -1)
         this.currentlang = lang.length > 0 ? lang[0] : all_langs[0];
         this.languages = Array.from(new Set(this.languages.concat(content_data.languages)));
       }
