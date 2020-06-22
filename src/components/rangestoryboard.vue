709ab51d57ec8f5e79b676441698ea88bdb174ec
Niqui O'Neill <doneill@ncsu.edu>
2020-06-22 17:19:28 -0400

    allow manifests to be fed into range

--- src/components/rangestoryboard.vue
+++ src/components/rangestoryboard.vue
@@ -1,6 +1,6 @@
 <template>
 <div v-bind:id="rangeid" class="rangestoryboard" v-bind:class="[!settings.fullpage && !isfullscreen ? 'rangestoryboardview' : 'rangefullpage']">
-  <storyboard :key="position" v-if="annotationurl" v-bind:annotationlist="annotationurl.anno" v-bind:manifesturl="annotationurl.manifest" v-bind:styling="stylingstring" v-bind:ws="isws" v-bind:layers="customlayers"></storyboard>
+  <storyboard :key="position" v-if="annotationurl" v-bind:jsonannotation="annotationurl.jsonanno" v-bind:annotationlist="annotationurl.anno" v-bind:manifesturl="annotationurl.manifest" v-bind:styling="stylingstring" v-bind:ws="isws" v-bind:layers="customlayers"></storyboard>
   <button id="previousPageInactiveButton" v-hotkey="prevshortcut" v-on:click="nextItemRange('prev')" class="pageButton toolbarButton" v-bind:class="[{ 'pageinactive' : prevPageInactive}, viewingDirection == 'rtl' ? 'floatleft' : 'floatright' ]">
     <span v-html="buttons.prev"></span>
     <span class="toolbartext">Previous page</span>
@@ -28,7 +28,8 @@ export default {
       'rangeurl':String,
       'styling': String,
       'ws': String,
-      'layers': String
+      'layers': String,
+      'manifesturl': String
     },
     data: function() {
       return {
@@ -68,36 +69,86 @@ export default {
     },
     created(){
       // get annotation urls in list
-      this.rangeid = this.$props.rangeurl.split("/").slice(-1)[0];
-      var isURL = shared.isURL(this.$props.rangeurl, this.settings);
+      this.rangeid = this.$props.rangeurl ? this.$props.rangeurl.split("/").slice(-1)[0] : this.$props.manifesturl.split("/").slice(-1)[0];
       this.settings = shared.getsettings(this);
-      if (isURL['isURL']){
-        axios.get(this.$props.rangeurl).then(response => {
-          this.getRangeData(response.data);
-        })
-      } else {
-        this.getRangeData(isURL['json'])
+      if (this.$props.rangeurl){
+        var isURL = shared.isURL(this.$props.rangeurl, this.settings);
+        if (isURL['isURL']){
+          axios.get(this.$props.rangeurl).then(response => {
+            this.getRangeData(response.data);
+          })
+        } else {
+          this.getRangeData(isURL['json'])
+        }
+      } else if (this.$props.manifesturl){
+        var isURL = shared.isURL(this.$props.manifesturl, this.settings);
+        if (isURL['isURL']){
+          axios.get(this.$props.manifesturl).then(response => {
+            this.getManifestData(response.data);
+          })
+        } else {
+          this.getManifestData(isURL['json'])
+        }
       }
     },
     methods: {
-      getRangeData: function(rangelist) {
-        var annos = rangelist.contentLayer.otherContent;
-        var canvases = rangelist.canvases ? rangelist.canvases : [];
-        var viewingDirection = rangelist.viewingDirection;
+      getManifestData: function(manifest) {
+        var otherContent = [];
+        if (manifest['sequences']){
+          canvases = [].concat.apply([], manifest['sequences'].map(element => element['canvases']));
+          for (var cv=0; cv<canvases.length; cv++){
+            var canvas = canvases[cv];
+            var canvasid = shared.getId(canvas);
+            if (canvas['otherContent']){
+              otherContent.push(canvas['otherContent'])
+            }            
+          }
+        } else {
+          otherContent = [].concat.apply([], manifest['items'].map(element => element['annotations']));
+        }
+        for (var an=0; an<otherContent.length; an++){
+          var anno = otherContent[an];
+          var type = 'url'
+          if(anno.constructor.name == 'String'){
+            var annourl = shared.getId(anno);
+          } else {
+            var jsonanno = anno;
+          }
+          var toclabel = anno['label'] ? anno['label'] : `Page ${an + 1}`
+          var description = anno['description'] ?  anno['description'] : '';
+          this.toc.push({ 'position' :an, 'label' : toclabel, 'description': description});
+          this.rangelist.push({'canvas': canvasid, 'anno': annourl, 'jsonanno':  jsonanno,'manifest': this.$props.manifesturl})
+        }
+        this.setDefaults(manifest)
+      },
+      setDefaults: function(data) {
+        var viewingDirection = data.viewingDirection;
         if(viewingDirection === 'right-to-left'){
           this.viewingDirection = 'rtl';
           this.buttons.prev = this.buttons.next;
           this.buttons.next = '<i class="fas fa-chevron-left"></i>';
         }
-        this.rangetitle = rangelist.label;
+        this.annotationurl = this.rangelist[0];
+        this.rangetitle = data.label['@value'] ? data.label['@value'] : data.label;
+        this.settings.autorun_interval ? '' : this.settings.autorun_interval = 3;
+        this.getTitle();
+        this.$props.ws ? this.isws = this.$props.ws : '';
+        this.$props.layers ? this.customlayers = this.$props.layers : '';
+        this.annotationurl.section ? this.settings.imagecrop = this.annotationurl.section : '';
+        this.getStylingString();
+        this.rangelist.length == 1 ? this.nextPageInactive = true : ''
+      },
+      getRangeData: function(rangelist) {
+        var annos = rangelist.contentLayer.otherContent;
+        var canvases = rangelist.canvases ? rangelist.canvases : [];
         for (var ca=0; ca<annos.length; ca++){
           var canvas = canvases[ca];
           var anno = annos[ca];
           var xywh = '';
           var manifest = canvas ? canvas['within'] : '';
-          manifest = manifest['@id'] ? manifest['@id'] : manifest['id'] ? manifest['id'] : manifest;
+          manifest = shared.getId(manifest);
           if (canvas){
-            var canvasid = canvas['@id'] ? canvas['@id'] : canvas['id'] ? canvas['id'] : canvas;
+            var canvasid = shared.getId(canvas);
             xywh = canvasid.constructor.name === 'String' && canvasid.split("#xywh=").length > 1 ? canvasid.split("#xywh=").slice(-1)[0] : '';
           }
           var annostring = anno['@id'] ? anno['@id'] : anno['id'] ? anno['id'] : anno;
@@ -106,15 +157,7 @@ export default {
           this.toc.push({ 'position' :ca, 'label' : toclabel, 'description': description});
           this.rangelist.push({'canvas': canvas, 'anno': annostring,  'manifest': manifest, section: xywh, title: anno['label']})
         }
-        this.annotationurl = this.rangelist[0];
-        // Get settings and create styling string
-        this.settings.autorun_interval ? '' : this.settings.autorun_interval = 3;
-        this.getTitle();
-        this.$props.ws ? this.isws = this.$props.ws : '';
-        this.$props.layers ? this.customlayers = this.$props.layers : '';
-        this.annotationurl.section ? this.settings.imagecrop = this.annotationurl.section : '';
-        this.getStylingString();
-        this.rangelist.length == 1 ? this.nextPageInactive = true : ''
+        this.setDefaults(rangelist);
       },
       nextItemRange: function(prevornext){
         if (prevornext == 'prev') {
