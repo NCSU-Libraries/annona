a787497c0984c7cd9c5f66259c7b7a8f99550042
Niqui O'Neill <doneill@ncsu.edu>
2021-06-17 17:58:46 -0400

    add in space

--- src/components/multistoryboard.vue
+++ src/components/multistoryboard.vue
@@ -22,7 +22,7 @@ export default {
         storyboard,
         toolbar
     },
-    props:{
+    props: {
       'manifesturl':String,
       'annotationurls': String,
       'annotationlists': String,
